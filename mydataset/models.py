9143e0730fcf1d273500b5898569b991815e7fee
Maria Tatiane <tatianemedeiros@ic.ufal.br>
2021-10-13 15:15:24 -0300

    import csv

--- mydataset/models.py
+++ mydataset/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+class Dataset (models.Model):
+    myid = models.IntegerField(_("myid"))
+    serie = models.BigIntegerField(_("serie"))
+    nome = models.CharField(_("nome"), max_length=150)
+    descricao = models.CharField(_("descrição"), max_length=255)
+    fonte = models.CharField(_("fonte"), max_length=255)
