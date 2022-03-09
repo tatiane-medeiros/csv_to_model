6f6d1a751b639c7870dfcb0b808b11c44bdd9196
Maria Tatiane <tatianemedeiros@ic.ufal.br>
2022-03-09 17:16:23 -0300

    pagination

--- mydataset/views.py
+++ mydataset/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.views import generic
+from django.core.paginator import Paginator
 from mydataset.models import Dataset
 
 # Create your views here.
@@ -8,5 +9,8 @@ from mydataset.models import Dataset
 def index(request):
     # return HttpResponse("Funcionou!!!")
     # template = loader.get_template('index.html')
-    dataset = Dataset.objects.all()
+    # dataset = Dataset.objects.all()
+    p = Paginator(Dataset.objects.all(), 8)
+    page = request.GET.get('page')
+    dataset = p.get_page(page)
     return render(request, 'mydataset/index.html', {'dataset': dataset})
