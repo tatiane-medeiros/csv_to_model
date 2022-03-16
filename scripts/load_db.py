3f4b4c850851b4954b949340cf8639f3c5dfbd24
Maria Tatiane <tatianemedeiros@ic.ufal.br>
2022-03-16 19:33:01 -0300

    load db script

---
+++ scripts/load_db.py
@@ -0,0 +1,11 @@
+from mydataset.models import Dataset
+import pandas as pd
+
+def run():
+    Dataset.objects.all().delete()
+    df = pd.read_csv('dataset.csv', sep=',')
+    df = df.fillna('')
+
+    for index, row in df.iterrows():
+        it = Dataset(serie=row['serie'], nome=row['nome'], descricao=row['descrição'], fonte=row['fonte'])
+        it.save()
